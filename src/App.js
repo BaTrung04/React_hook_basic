dee31b97189f2b8eeea1d13465d781a091afc2dc
Ba Trung <batrung0410job@gmail.com>
2023-11-24 01:54:38 +0700

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./views/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>REACT HOOK</h1>
-      </header>
-    </div>
-  );
-}
+//template + logic
+// JSX
+// Babel
+
+const App = () => {
+    return (
+        <div className="App">
+            <Nav />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>REACT HOOK</h1>
+            </header>
+        </div>
+    );
+};
 
 export default App;
