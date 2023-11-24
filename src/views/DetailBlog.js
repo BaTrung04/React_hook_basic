31d4f4c78c9e9469a224ad4a2d84ed8d3e52a62c
Ba Trung <batrung0410job@gmail.com>
2023-11-24 21:59:18 +0700

    reise hook

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -2,19 +2,41 @@ import {
     useParams,
     useHistory,
 } from "react-router-dom/cjs/react-router-dom.min";
+import useFetch from "../customize/fetch";
+import "./Blog.scss";
 
 const DetailBlog = () => {
     let { id } = useParams();
     let history = useHistory();
+
+    const {
+        data: dataBlogDetail,
+        isLoading,
+        isError,
+    } = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false);
+
     const handleBackData = () => {
         history.push("/blog");
     };
+    console.log();
     return (
         <>
             <div>
                 <span onClick={() => handleBackData()}>&lt;--- Back</span>
             </div>
-            <h1>Hello DetailBlog {id}</h1>
+            <div className="Blog-Detail">
+                {dataBlogDetail && (
+                    <>
+                        <div className="title">
+                            Blog id:{id} ---
+                            {isLoading === true
+                                ? "Loaning data.... "
+                                : dataBlogDetail.title}
+                        </div>
+                        <div className="content">{dataBlogDetail.body}</div>
+                    </>
+                )}
+            </div>
         </>
     );
 };
